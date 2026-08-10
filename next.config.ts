e6d040f7df6152447e1b1925a8a373e66d929f62
Lisandro Barbosa <lisandrobarbosa669@gmail.com>
2026-08-10 11:50:53 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/filtech-website",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
